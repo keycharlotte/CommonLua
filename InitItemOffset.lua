7a117289d7910352bf99043a2b3b23c5eda600ad
keycharlotte <421602663@qq.com>
2017-09-15 13:49:26 +0800

    Create InitItemOffset.lua

---
+++ InitItemOffset.lua
@@ -0,0 +1,7 @@
+	local Count = #List
+	for i=1,#List do
+		local Item = resMgr:CreateSprite("xxx")
+		Item:SetPosition((2*i-Count+1)*Item:GetSize().x/2,0) --horizontal
+    Item:JoinIn(Panel)
+    self.XXXList[i] = Item
+	end
