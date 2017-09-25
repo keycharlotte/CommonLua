b88452c222ea9d146ebe1ee952a49f9aa16b8562
keycharlotte <421602663@qq.com>
2017-09-25 10:44:17 +0800

    Update InitItemOffset.lua

--- InitItemOffset.lua
+++ InitItemOffset.lua
@@ -2,7 +2,7 @@
 	local Count = #List
 	for i=1,#List do
 		local Item = resMgr:CreateSprite("xxx")
-		Item:SetPosition((2*i-Count-1)*Item:GetSize().x/2,0) --horizontal
+		Item:SetPosition(((2*i-Count-1)*Item:GetSize().x/2+offset),0) --horizontal
 		Item:JoinIn(Panel)
 		self.XXXList[i] = Item
 	end
